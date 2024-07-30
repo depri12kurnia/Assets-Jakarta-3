d0f9fd5c35674c1e9047ccad2c74518e6ad7c2ac
Depri Kurnia <42798521+depri12kurnia@users.noreply.github.com>
2024-07-30 16:53:37 +0700

    Add files via upload

---
+++ floating/floating.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('.floating-widget').addEventListener('click', function() {
+    // Contoh: Arahkan ke URL atau buka modal
+    window.open('https://sippn.menpan.go.id/', '_blank'); // Ganti URL dengan link yang diinginkan
+});
